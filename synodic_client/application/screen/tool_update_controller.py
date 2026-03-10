46aaab4286a5d69d051e52aeb2d439b8e2dc0f8c
Asher Norland <asher.norland@gmail.com>
2026-03-10 12:03:36 -0700

    Update Chore (#62)

--- synodic_client/application/screen/tool_update_controller.py
+++ synodic_client/application/screen/tool_update_controller.py
@@ -438,7 +438,7 @@ class ToolUpdateOrchestrator:
         # not None) call show() below which triggers the refresh.
         tools_view = self._window.tools_view
         if tools_view is not None:
-            tools_view._updates_checked = False
+            tools_view.invalidate_update_data()
             if self._window.isVisible() and target is None:
                 tools_view.refresh()
 
@@ -509,7 +509,10 @@ class ToolUpdateOrchestrator:
         except Exception as exc:
             logger.exception('Package removal failed')
             self._fail_package_update(
-                plugin_name, package_name, f'Failed to remove {package_name}: {exc}', removing=True,
+                plugin_name,
+                package_name,
+                f'Failed to remove {package_name}: {exc}',
+                removing=True,
             )
 
     def _on_package_remove_finished(
@@ -523,7 +526,10 @@ class ToolUpdateOrchestrator:
             detail = result.message or 'Unknown error'
             logger.warning('Package removal failed for %s/%s: %s', plugin_name, package_name, detail)
             self._fail_package_update(
-                plugin_name, package_name, f'Could not remove {package_name}: {detail}', removing=True,
+                plugin_name,
+                package_name,
+                f'Could not remove {package_name}: {detail}',
+                removing=True,
             )
             return
 
@@ -532,5 +538,5 @@ class ToolUpdateOrchestrator:
         tools_view = self._window.tools_view
         if tools_view is not None:
             tools_view.set_package_removing(plugin_name, package_name, False)
-            tools_view._updates_checked = False
+            tools_view.invalidate_update_data()
             tools_view.refresh()
